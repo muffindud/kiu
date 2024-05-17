1b21fab231574edb49c50d0026676f0717eb5ed7
muffindud <ccatlabuga2002@gmail.com>
2024-05-17 12:47:31 +0300

    add addElement option

--- src/components/AddQueueForm/AddQueueForm.jsx
+++ src/components/AddQueueForm/AddQueueForm.jsx
@@ -2,11 +2,22 @@ import { useState } from 'react';
 import './AddQueueForm.css'
 
 function AddQueueForm({ setFormState, handleAddQueue }) {
+  const [elementsCount, setElementsCount] = useState(1);
+
   const [queueData, setQueueData] = useState({
     name: '',
     description: '',
     queue_list: []
   });
+
+  const handleAddElement = (e) => {
+    setQueueData({...queueData, queue_list: [...queueData.queue_list, e.target.value]});
+  };
+
+  const handleRemoveElement = (index) => {
+    const newQueueList = queueData.queue_list.filter((item, i) => i !== index);
+    setQueueData({...queueData, queue_list: newQueueList});
+  };
 
   return (
     <div className='add-queue-form-container'>
@@ -39,12 +50,34 @@ function AddQueueForm({ setFormState, handleAddQueue }) {
             />
           </div>
 
+          {Array.from({length: elementsCount}, (_, i) => (
+            <div className='form-element' key={i}>
+              <input
+                type='text'
+                required
+                onChange={handleAddElement}
+              />
+              <button
+                type='button'
+                onClick={() => handleRemoveElement(i)}
+              >
+                Remove
+              </button>
+            </div>
+          ))}
+
           <div className='form-buttons'>
             <button 
               type='button'
               onClick={() => setFormState(false)}
             >
               Cancel
+            </button>
+            <button
+              type='button'
+              onClick={() => setElementsCount(elementsCount + 1)}
+            >
+              Add Element
             </button>
             <button 
               type='submit'
